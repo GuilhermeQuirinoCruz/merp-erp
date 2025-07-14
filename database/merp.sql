a1c029138bed50cfc5f4866269613e4a412512eb
Guilherme Quirino <guilhermequirinocruz@gmail.com>
2025-07-14 11:40:24 -0300

    atualização do sql e .gitignore

--- database/merp.sql
+++ database/merp.sql
@@ -2,30 +2,30 @@ CREATE DATABASE merp;
 
 \c merp
 
-CREATE TABLE produto (
+CREATE TABLE product (
     id INT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    preco DECIMAL(10,2) NOT NULL,
-    estoque INT NOT NULL
+    tag VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock INT NOT NULL
 );
 
-CREATE TABLE cupom (
+CREATE TABLE coupon (
     id INT PRIMARY KEY,
-    codigo VARCHAR(100) NOT NULL,
-    vencimento DATE NOT NULL,
-    porcentagem_desconto DECIMAL(10,2) NOT NULL
+    code VARCHAR(100) NOT NULL,
+    expires_at DATE NOT NULL,
+    discount_percentage DECIMAL(10,2) NOT NULL
 );
 
-CREATE TABLE pedido (
+CREATE TABLE order (
     id INT PRIMARY KEY,
-    data DATE NOT NULL,
-    frete DECIMAL(10,2) NOT NULL,
+    ordered_at DATE NOT NULL,
+    delivery_tax DECIMAL(10,2),
     total DECIMAL(10,2) NOT NULL,
-    desconto_cupom DECIMAL(10,2) NOT NULL
+    coupon_discount DECIMAL(10,2)
 );
 
-CREATE TABLE produto_pedido (
-    id_pedido INT REFERENCES pedido(id) NOT NULL,
-    nome VARCHAR(100) NOT NULL,
-    preco DECIMAL(10,2) NOT NULL
+CREATE TABLE order_product (
+    order_id INT REFERENCES pedido(id) NOT NULL ON DELETE CASCADE,
+    tag VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL
 );
