373867463ab59496486679b8cb8961e9fff8440d
Guilherme Quirino <guilhermequirinocruz@gmail.com>
2025-07-17 17:45:50 -0300

    testes de validação de produto

--- backend/src/test/product.test.ts
+++ backend/src/test/product.test.ts
@@ -1,13 +1,46 @@
-import { expect, test, describe } from "vitest";
-import { getProduct, getAllProducts } from "../product/productController.js";
+import { expect, test, describe, afterEach, beforeEach } from "vitest";
+import { getErrorsWithProduct } from "../product/productController.js";
+import { Product, getEmptyProduct } from "../model/product.js";
 
-describe("post requests", () => {
-  test("returns 1", () => {});
-});
+function resetProduct(product: Product) {
+  product.id = 0;
+  (product.tag = ""), (product.price = 0);
+  product.stock = 0;
+}
 
-describe("get requests", () => {
-  test("get all", async () => {
-    
-    expect(await getAllProducts()).toBe(1);
+describe("Product validation", async () => {
+  let product: Product = getEmptyProduct();
+  beforeEach(() => {
+    resetProduct(product);
+  });
+
+  test("Should reject product with no tag", () => {
+    expect(getErrorsWithProduct(product)).toContain("Tag não pode ser vazia");
+  });
+
+  test("Should reject product with a tag length greater than allowed", () => {
+    for (let i = 0; i < 10; i++) {
+      product.tag += "0123456789-";
+    }
+
+    expect(getErrorsWithProduct(product)).toContain("Tag muito longa");
+  });
+
+  test("Should reject product with no price", () => {
+    expect(getErrorsWithProduct(product)).toContain(
+      "O produto precisa de um preço"
+    );
+  });
+
+  test("Should reject product with negative price or stock", () => {
+    product.price = -1;
+    expect(getErrorsWithProduct(product)).toContain(
+      "O preço deve ser um número positivo"
+    );
+
+    product.stock = -1;
+    expect(getErrorsWithProduct(product)).toContain(
+      "O estoque deve ser um número positivo"
+    );
   });
 });
